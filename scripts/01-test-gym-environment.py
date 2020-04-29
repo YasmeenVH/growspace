0509f16aa857150a28835ae44472c6cef068d036
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2020-04-29 17:34:44 -0400

    testing env

---
+++ scripts/01-test-gym-environment.py
@@ -0,0 +1,5 @@
+### File to test if loading env correctly with no error or any outputs!
+
+import gym
+import growspace                            #load custom env
+env = gym.make("GrowSpaceEnv-States-v0")
