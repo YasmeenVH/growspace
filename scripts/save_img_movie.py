d33fb3410cc8eae43b518de3c816f01cc7cfec0e
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2020-06-17 15:38:13 -0400

    movie maker

---
+++ scripts/save_img_movie.py
@@ -0,0 +1,33 @@
+import gym
+import time
+import growspace
+import numpy as np
+import matplotlib.pyplot as plt
+import os, sys
+import imageio
+import cv2
+
+env = gym.make("GrowSpaceEnv-Images-v0")
+
+def run_episode():
+    env.reset()
+    for step in range(25):        # defined in __init__.py    # here we're timing the step function
+        env.step(env.action_space.sample())
+        img = env.get_observation(debug_show_scatter=True)
+        path = '/home/y/Documents/finalprojectcomp767/growspace/scripts/png/'
+        cv2.imwrite(os.path.join(path,'step'+str(step)+'.png'),img)
+
+
+def save_file_movie():
+    png_dir = '/home/y/Documents/finalprojectcomp767/growspace/scripts/png/'
+    images = []
+    for file_name in os.listdir(png_dir):
+        if file_name.endswith('.png'):
+            file_path = os.path.join(png_dir, file_name)
+            images.append(imageio.imread(file_path))
+    imageio.mimsave('../scripts/movie/movie.gif', images,fps=1)
+
+if __name__ == '__main__':
+  #run_episode()
+  save_file_movie()
+
