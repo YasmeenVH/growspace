42ebc088fc5243106168356c5c87f192deebaec5
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2020-07-17 15:22:49 -0400

    change in node addition

--- growspace/data_structure/search_tree.py
+++ growspace/data_structure/search_tree.py
@@ -1,27 +1,44 @@
-
-class Node(object):
-    def __init__(self, value=None):
-        self.value = value
+class Node:
+    def __init__(self, value):
+        self.data = value
         self.left = None
         self.right = None
 
-class BST(object):
+    def insert(self, data):
+        if self.data == data:
+            return False
+        elif self.data > data:
+            if self.left:
+                return self.left.insert(data)
+            else:
+                self.left = Node(data)
+                return True
+
+        else:
+            if self.right:
+                return self.right.inser(data)
+            else:
+                self.right = Node(data)
+                return True
+
+class BST:
     def __init__(self):
         self.root = None
 
-    def insert(self, data):
-        if self.root is None:
-            self.root = Node(data)
+    def insert(self, node):
+        # new_node = Node(node)
+        if self.root:
+            self.root.insert(node)
         else:
-            self._insert(data, self.root)
+            self.root = Node(node)
 
     def _insert(self, data, cur_node):
-        if data[0] < cur_node.value[0]:
+        if data[0] < cur_node.data[0]:
             if cur_node.left is None:
                 cur_node.left = Node(data)
             else:
                 self._insert(data, cur_node.left)
-        elif data[0] >= cur_node.value[0]:       # allow for duplicates since we may have same X values
+        elif data[0] >= cur_node.data[0]:  # allow for duplicates since we may have same X values
             if cur_node.right is None:
                 cur_node.right = Node(data)
             else:
@@ -29,7 +46,7 @@ class BST(object):
         else:
             print('The X value does not fit in the tree')
 
- # no need for this now
+    # no need for this now
 
     def find(self, data):
         if self.root:
@@ -41,7 +58,7 @@ class BST(object):
         else:
             return None
 
-# no need for this now
+    # no need for this now
 
     def _find(self, data, cur_node):
         if data > cur_node.data and cur_node.right:
@@ -57,6 +74,7 @@ class BST(object):
             return
 
         if r1 < cur_node.value[0]:
+            print("if bigger than than r1: ", cur_node.value[0])
             self.key_range(cur_node.left, r1, r2)
 
         if r1 <= cur_node.value[0] <= r2:
@@ -64,10 +82,11 @@ class BST(object):
             return keys
 
         if r2 > cur_node.value[0]:
+            print("if smaller than r2: ", cur_node.value[0])
             self.key_range(cur_node.right, r1, r2)
 
-## no need for the recursive _keyrange function
-    def _key_range(self, cur_node, r1,r2):
+    ## no need for the recursive _keyrange function
+    def _key_range(self, cur_node, r1, r2):
         keys = []
 
         if cur_node is None:
@@ -83,7 +102,11 @@ class BST(object):
             self._key_range(cur_node.right, r1, r2)
 
 
-
-
-
-
+if __name__ == '__main__':
+    tree = BST()
+    root = tree.insert([20, 20])
+    print(root)
+    tree.insert([3, 20])
+    tree.insert([2, 30])
+    xs = tree.key_range(root, 1, 10)
+    print(xs)
