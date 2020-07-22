f9337192e269569b874516fd663fa6ed62faaf6a
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2020-07-22 11:52:10 -0400

    metrics file for measuring

---
+++ scripts/05-env-metrics.py
@@ -0,0 +1,16 @@
+import gym
+import time
+import growspace
+import numpy as np
+import matplotlib.pyplot as plt
+
+env = gym.make("GrowSpaceEnv-Images-v0")
+
+
+def distance_to_original_tip():
+
+
+def number_of_branches():
+
+
+def
