7b80bc42f57aa4275398e5640a26d14a99cd6352
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2020-08-18 23:48:20 -0400

    clean and ready to plot with any number of steps

--- scripts/random_solver.py
+++ scripts/random_solver.py
@@ -10,55 +10,22 @@ env = gym.make("GrowSpaceEnv-Images-v0")
 
 
 
-def random_solver():
+def random_solver(num_steps):
 
     state_space = np.zeros((84, 84))
     print(state_space)
     env.seed()
     env.reset()
-    # it +=1
+
     count = 0
     rewards = []
-    timers_step = []
-    # dict_of_tips = SortedDict()
-    actions = [0, 1, 2]
-    b_tips = set()
-    for s in range(30):  # same as ppo
 
-        if s == 0:  # first step
-            s_t = env.step(env.action_space.sample())  ### s_t --> tips, target, light
-            tips = s_t[3]['tips']  # add seperately as they are first set
-            b_tips.add(tips)  # put in set for unique values
+    for s in range(num_steps):  # same as ppo
 
-        else:
-            tips = s_t[3]['tips']
-        n_tips_to_add = len(tips) - len(b_tips)
-        b_tips.add(tips[:-n_tips_to_add])  # add last tips of list
-        x_range = [i[0] for i in tips[:-n_tips_to_add]]  # find range of x for linking reward
-        y_range = [i[1] for i in tips[:-n_tips_to_add]]
-        dist = distance.cdist(coords, s_t[3]['target'], 'euclidean')
-        pick
-        euclidean
-        distance
-        look
-        at
-        branch
-        positionement
-        within
-        light
-        range
-        decide if light is to
-        move
-        reward = s_t[1]
-        print(x_range)
+        s_t = env.step(env.action_space.sample())  ### s_t --> tips, target, light
+        rewards.append(s_t[1])
 
-        # add reward to statespace matrix
-        # add
+    return rewards
 
-        s_t = env.step(action_t)
-
-        action_t
-        picke
-        print(tips)
-
-#branches_trimmed = sample(self.branches, MAX_BRANCHING)
+if __name__ == '__main__':
+    random_solver(40)
