35939959912c80f0437a3c4951906af04a0886aa
manuel <manuel.delverme@gmail.com>
2021-03-02 12:43:34 +0100

    Split water and continuous

--- growspace/envs/growspace_continuous.py
+++ growspace/envs/growspace_continuous.py
@@ -1,5 +1,4 @@
 import enum
-import random
 
 import cv2
 import gym
@@ -24,80 +23,10 @@ def to_int(v):
     return int(round(v))
 
 
-class Actions(enum.IntEnum):
-    move_left = 0
-    move_right = 1
-    increase_beam = 2
-    decrease_beam = 3
-    noop = 4
-    water = 5
-
-
-class GrowSpaceWater(GrowSpaceEnv_Control):
-    def __init__(self):
-        self.max_water_level = 1
-        self.water_level = self.max_water_level
-        self.branch_water_usage = 0.1
-        self.paint_water = False
-        super().__init__()
-        self.action_space = self.action_space.__class__(self.action_space.n)
-
-    def step(self, action):
-        resource_cost = 0.
-        self.paint_water = False
-        if action == Actions.water:
-            self.water_level = self.max_water_level
-            action = Actions.noop
-            resource_cost += 1.
-            self.paint_water = True
-
-        s, reward, t, i = super().step(action)
-
-        reward -= resource_cost
-        if i['new_branches']:
-            self.water_level -= i['new_branches'] * self.branch_water_usage
-        return s, reward, t, i
-
-    def get_observation(self, *args, **kwargs):
-        obs = super().get_observation(*args, **kwargs)
-        if self.paint_water:
-            obs[-5:-1, -5:-1, 0] = 255
-        return obs
-
-    def tree_grow(self, *args, **kwargs):
-        if self.water_level <= 0:
-            return [[branch.x2, branch.y2] for branch in self.branches]
-
-        return super().tree_grow(*args, **kwargs)
-
-    def reset(self):
-        self.water_level = self.max_water_level
-        super().reset()
-
-    def render(self, mode='human',
-               debug_show_scatter=False):  # or mode="rgb_array"
-        img = self.get_observation(debug_show_scatter)
-
-        if self.obs_type == 'Binary':
-            image = img.astype(np.uint8)
-            img = image * 255
-
-        if mode == "human":
-            cv2.imshow('plant', img)  # create opencv window to show plant
-            cv2.waitKey(1)  # this is necessary or the window closes immediately
-        else:
-            return img
-
-
 class ContinuousActions(enum.IntEnum):
     light_velocity = 0
     beam_width = 1
     # water = 2 # TODO add water -> 3 actions
-
-
-import warnings
-
-warnings.warn("remember to implement continuous water")
 
 
 class GrowSpaceContinuous(GrowSpaceEnv_Control):
@@ -138,25 +67,8 @@ class GrowSpaceContinuous(GrowSpaceEnv_Control):
 
 
 if __name__ == '__main__':
-
-    # env = GrowSpaceWater()
     env = GrowSpaceContinuous()
 
-    def key2action(key):
-        if key == ord('a'):
-            return 0  # move left
-        elif key == ord('d'):
-            return 1  # move right
-        elif key == ord('s'):
-            return 4  # stay in place
-        elif key == ord('w'):
-            return 2
-        elif key == ord('x'):
-            return 3
-        elif key == ord('e'):
-            return 5
-        else:
-            return None
 
     def key2continuous_action(key):
         if key == ord('a'):
