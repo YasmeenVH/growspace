514e81e355ec624e6b6695138a8790e34fb68795
YasmeenVH <yasmeen.hitti@mail.mcgill.ca>
2021-05-31 18:29:32 -0400

    removed comment

--- growspace/envs/growspace_control.py
+++ growspace/envs/growspace_control.py
@@ -53,7 +53,7 @@ class Features(IntEnum):
     scatter = 1
 
 class GrowSpaceEnv_Control(gym.Env):
-    def __init__(self, width=DEFAULT_RES, height=DEFAULT_RES, light_dif=LIGHT_DIF, obs_type = None, level = None, setting = 'easy'):
+    def __init__(self, width=DEFAULT_RES, height=DEFAULT_RES, light_dif=LIGHT_DIF, obs_type = None, level = 'second', setting = 'easy'):
         self.width = width  # do we keep?
         self.height = height  # do we keep?
         self.seed()
@@ -382,101 +382,18 @@ class GrowSpaceEnv_Control(gym.Env):
 
 
         pts = self.light_scatter()
-        #     if len(convex_tips) >= 2:
-        #         #print('check')
-        #         hull = ConvexHull(convex_tips)
-        #         #print('check2')
-        #         #print('what is this:',convex_tips[hull.vertices,0])
-        #        # print('what is this8:', convex_tips[hull.vertices, 1])
-        #         xxs = convex_tips[hull.vertices, 0]  # x coords for convex hull around tips
-        #         yys = convex_tips[hull.vertices, 1]  # y coords for convex hull around tips
-        #         x_max_idx = np.where(xxs == np.amax(xxs))  # idx where most right tip is
-        #         x_min_idx = np.where(xxs == np.amin(xxs))  # idx where most left tip is
-        #         y_max_idx = np.where(yys == np.amax(yys))  # idx highest tip
-        #         #print(x_max_idx[0], 'this is the idx')
-        #         #x_min_idx = np.where(min(xxs))
-        #         y_max = xxs[y_max_idx]
-        #         #print(xs,'this is xs')
-        #
-        #         if self.x1_light < xxs[x_min_idx] < self.x2_light:  # if within the horizontal beam
-        #
-        #             if self.x1_light < xxs[x_max_idx] < self.x2_light:
-        #
-        #                 # full convex is in within the beam
-        #                 if yys[x_min_idx] < yys[x_max_idx]:
-        #                     if xxs[x_min_idx] < xxs[x_max_idx]:
-        #                         filter1 = np.logical_and(xs >= xxs[x_min_idx], xs <= xxs[x_max_idx])
-        #                         filter2 = np.logical_and(ys >= 0, ys <= yys[x_min_idx])
-        #                     else:
-        #                         filter1 = np.logical_and(xs >= xxs[x_max_idx], xs <= xxs[x_min_idx])
-        #                         filter2 = np.logical_and(ys >= 0, ys <= yys[x_max_idx])
-        #
-        #             # convex is on right side and in between beam
-        #             filter1 = np.logical_and(xs >= xxs[x_min_idx], xs <= self.x2_light)
-        #             filter2 = np.logical_and(ys <= yys[x_min_idx], ys >= 0)
-        #
-        #             # Filtering for values that are not shaded
-        #             idx = [i for i in range(len(filter1)) if (filter1[i] == False) and (filter2[i] == False)]
-        #             xs = [xs[i] for i in idx]
-        #             ys = [ys[i] for i in idx]
-        #
-        #         elif self.x1_light < xxs[x_max_idx] < self.x2_light:
-        #
-        #             # convex is on left side and in between beam
-        #             filter1 = np.logical_and(xs <= xxs[x_max_idx], xs >= self.x1_light)
-        #             filter2 = np.logical_and(ys >= 0,ys <= yys[x_max_idx])
-        #
-        #             # Filtering for values that are not shaded
-        #             idx = [i for i in range(len(filter1)) if (filter1[i] == False) and (filter2[i] == False)]
-        #             xs = [xs[i] for i in idx]
-        #             ys = [ys[i] for i in idx]
-        #
-        #         elif xxs[x_min_idx] < self.x1_light < self.x2_light < xxs[x_max_idx]:
-        #             # width of light is covering convex with no exposed edges
-        #             intersection_1 = []
-        #             intersection_2 = []
-        #
-        #             for i in range(len(xxs)):
-        #                 # find intersecting vertex with convex
-        #                 if xxs[i+1]< self.x1_light < xxs[i]:
-        #                     intersection_1.append(xxs[i], xxs[i+1])
-        #                     intersection_1.append(yys[i], yys[i+1])
-        #
-        #                 if xxs[i+1] < self.x2_light < xxs[i]:
-        #                     intersection_2.append(xxs[i], xxs[i+1])
-        #                     intersection_2.append(yys[i], yys[i+1])
-        #                     #print('what is intersection2',intersection_2)
-        #             y1 = intersection_(intersection_1,self.x1_light)
-        #             y2 = intersection_(intersection_2,self.x2_light)
-        #             y_limit = min(y1,y2)
-        #             filter1 = np.logical_and(xs <= self.x1_light, xs >= self.x2_light)
-        #             filter2 = np.logical_and(ys >= 0, ys <= y_limit)
-        #
-        #             idx = [i for i in range(len(filter1)) if (filter1[i] == False) and (filter2[i] == False)]
-        #             #print("what is idx", idx)
-        #             xs = [xs[i] for i in idx]
-        #             ys = [ys[i] for i in idx]
-        #
-        #         else:
-        #             pass #this  is when beam is not covering plants
-        # except:
-        #     pass
-
-
 
         tips = self.tree_grow(pts, .01 * self.width, .15 * self.height)
         self.draw_beam()
 
         # Calculate distance to target
-        d = self.distance_target(tips)
-        #d = 1/d
-        #print('distance', d)
+  
         if self.distance_target(tips) <= 3.2: # before was 0.1
-            reward = 1/3.2#0.1 /10
+            reward = 1/3.2 #0.1 /10
             success = 1
 
         else:
-            reward = 1 / self.distance_target(tips) #/10
+            reward = 1 / self.distance_target(tips) #adsss/10
             success = 0
 
         # Render image of environment at current state
@@ -557,6 +474,7 @@ if __name__ == '__main__':
         else:
             return None
     rewards = []
+    rewards_mean = []
     while True:
         gse.reset()
         img = gse.get_observation(debug_show_scatter=False)
@@ -571,8 +489,12 @@ if __name__ == '__main__':
             b,t,c,f = gse.step(action)
             #print(f["new_branches"])
             rewards.append(t)
-            print(t)
+            #print(t)
             cv2.imshow("plant", gse.get_observation(debug_show_scatter=False))
         total = sum(rewards)
 
+        rewards_mean.append(total)
+        av = np.mean(rewards_mean)
+        print("amount of rewards:", total)
+        print('mean:', av)
         print("amount of rewards:", total)
